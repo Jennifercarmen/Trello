8db36c1a4b509d924bb8686a528b19040851eb83
Jennifer Carmen <32302079+Jennifercarmen@users.noreply.github.com>
2017-11-21 10:44:38 -0500

    añadiendo form

--- js/app.js
+++ js/app.js
@@ -1,43 +1,65 @@
 window.addEventListener("load", function () {
 	// Declaramos variables a usar
 	var contenedor = document.getElementById("contenedor");
-	var btn = document.getElementById("button_tarjeta");
-	var form_card = document.getElementById("tarjeta");
+	var btn_lista = document.getElementById("button_lista");
 	var tarjeta = document.getElementById("tinput");
-	var form_card = document.getElementById("añade_tarjeta");
+	var contentcard = document.getElementById("añade_tarjeta");
 	var guardarTarjeta = document.getElementById("btnGuardar");
 	//ocultamoe el contenedor de añadir tarjeta
-	form_card.style.display = "none";
-	btn.addEventListener("click", function (event) {
-		// ocultamos boton que contiende evento agregar tarjeta
-		btn.style.display = "none";
-		// posicionamos el nuevo contenedor de la nueva tarjeta
-		form_card.style.display = "inline-block";
+	contentcard.style.display = "none";
+
+	btn_lista.addEventListener("click", function (event) {
+		// ocultamos boton que contiene evento agregar tarjeta
+		btn_lista.style.display = "none";
+		//Mostramos contentcard como un contenedor de bloque de nivel en línea.
+		contentcard.style.display = "inline-block";
 		tarjeta.value = "";
 		tarjeta.focus;
 	});
+
 	// Manejador de evento del boton guardar tarjeta
 	guardarTarjeta.addEventListener("click", function (event) {
 		// volvemos a ocultar el contenedor de añadir tarjeta
-		form_card.style.display = "none";
+		contentcard.style.display = "none";
 		var tarjetas = document.createElement("div");
-		var tarjeta = document.getElementById("tinput");
 		var title = document.createElement("div");
 		title.innerHTML = tarjeta.value;
 		// añadimos contenedores tarjetas uno delante del otro en este caso los div 
 		// estaran posicionados en linea y se posicionara a la izquierda del elemento
 		contenedor.insertBefore(tarjetas, contenedor.lastElementChild);
 		tarjetas.appendChild(title);
-		var addhomework = document.createElement("div");
-		addhomework.innerHTML = "Agrega una nueva tarjeta";
+		var addcard = document.createElement("div");
+		addcard.innerHTML = "Agrega una nueva tarjeta";
 		// añadimos clase a los elementos
 		tarjetas.classList.add("card");
-		addhomework.classList.add("add_card");
+		addcard.classList.add("add_card");
 		title.classList.add("title");
-		tarjetas.appendChild(addhomework);
+		tarjetas.appendChild(addcard);
 		var btn_addcard = document.querySelectorAll(".add_card");
-		btn.style.display = "inline-block";
-
+		btn_lista.style.display = "inline-block";
+		//Manejador de evento click para el div añadir tarjeta
+		addcard.addEventListener("click", function (event) {
+			addcard.style.display = "none";
+			var form = document.createElement("form");
+			tarjetas.appendChild(form);
+			var textarea = document.createElement("textarea");
+			textarea.classList.add("textarea");
+			form.appendChild(textarea);
+			var btnaddcard = document.createElement("button");
+			btnaddcard.classList.add("btnAnadir");
+			btnaddcard.innerHTML = "Añadir";
+			form.appendChild(btnaddcard);
+		
+			btnaddcard.addEventListener("click", function (event) {
+				event.preventDefault();
+				addcard.style.display = null;
+				var textCard = document.createElement("div");
+				textCard.classList.add("divcard");
+				textCard.innerHTML = textarea.value;
+				tarjetas.insertBefore(textCard, addcard);
+				textarea.value = "";
+				form.style.display = "none";
+			});
+		});
 	});
-
 });
